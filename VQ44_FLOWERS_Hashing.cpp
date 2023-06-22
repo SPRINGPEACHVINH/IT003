794fb8072e2dc4d16e589f4ac12dca591b354fbf
SPRINGPEACHVINH <daoxuan.vinh3@gmail.com>
2023-06-22 13:31:45 +0700

    commit

--- VQ44_FLOWERS_Hashing.cpp
+++ VQ44_FLOWERS_Hashing.cpp
@@ -7,11 +7,54 @@ vector<int> a;
 vector<int> get_ans(const vector<int> &A, int K)
 {
     map<int, int> Hash;
+    vector<int> ans;
+    int index = 1;
     for (int i = 0; i < n; i++)
     {
-        Hash[a[i]]++;
+        auto it = Hash.find(a[i]);
+        if (it != Hash.end())
+        {
+            int temp = it->first;
+            temp++;
+            Hash.erase(it);
+            Hash.insert(make_pair(temp, A[i]));
+        }
+        else
+            Hash.insert(make_pair(index, A[i]));
     }
-    
+    int num = Hash.size();
+    if (k > num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            auto it = Hash.begin();
+            ans.push_back(it->second);
+            Hash.erase(it);
+        }
+        for (int i = k - num; i < k; i++)
+        {
+            ans.push_back(Hash.begin()->second);
+        }
+    }
+    else if (k == num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            auto it = Hash.begin();
+            ans.push_back(it->second);
+            Hash.erase(it);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < k; i++)
+        {
+            auto it = Hash.begin();
+            ans.push_back(it->second);
+            Hash.erase(it);
+        }
+    }
+    return ans;
 }
 
 int main()
