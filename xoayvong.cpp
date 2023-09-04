9d3e85bf6f2345f4e37776bd5ca95718aa58e103
SPRINGPEACHVINH <daoxuan.vinh3@gmail.com>
2023-09-04 14:33:23 +0700

    commit

--- xoayvong.cpp
+++ xoayvong.cpp
@@ -2,22 +2,27 @@
 #include <math.h>
 #include <vector>
 using namespace std;
-int main() {
-    long n,k;
-    cin>>n>>k;
+int main()
+{
+    long n, k;
+    cin >> n >> k;
     int a[n];
-    for(int i=0; i<n; i++) {
-        cin>>a[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
     }
-    k%=n;
-    vector <int> arr;
-    for(int i=n-k; i<n; i++) {
+    k %= n;
+    vector<int> arr;
+    for (int i = n - k; i < n; i++)
+    {
         arr.push_back(a[i]);
     }
-    for(int i=0; i<n-k; i++) {
+    for (int i = 0; i < n - k; i++)
+    {
         arr.push_back(a[i]);
     }
-    for(int i=0; i<arr.size(); i++) {
-        cout<<arr[i]<<" ";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
     }
 }
